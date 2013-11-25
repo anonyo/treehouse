02d0dbf41ec674e715cbf04360e1333ed4010373
Anonyo Hossain <anonyo.hossain@gmail.com>
2013-11-25 01:27:58 +0000

    added flash and alert messages

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,6 +1,7 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
+  
   test "a user should enter a first name" do
     user = User.new
     assert !user.save
@@ -18,4 +19,21 @@ class UserTest < ActiveSupport::TestCase
     assert !user.save
     assert !user.errors[:profile_name].empty?
   end
+  
+  test "a user should have an unique profile name" do
+    user = User.new
+    user.profile_name = users(:anonyo).profile_name
+    assert !user.save
+    puts user.errors.inspect
+    assert !user.errors[:profile_name].empty?
+  end
+  
+  test "a user should have a profile name without spaces" do
+    user = User.new
+    user.profile_name = users(:anonyo).profile_name
+    assert !user.save
+    assert !user.errors[:profile_name].empty?
+    assert user.errors[:profile_name].include?("Must be formatted correctly.")
+  end
+  
 end
