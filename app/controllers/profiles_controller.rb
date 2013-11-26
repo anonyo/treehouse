0e299ffaa6b1f7c6065876f9191c0f33668e81f2
Anonyo Hossain <anonyo.hossain@gmail.com>
2013-11-26 15:16:08 +0000

    did some TDD

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,4 +1,10 @@
 class ProfilesController < ApplicationController
   def show
+    @user = User.find_by_profile_name(paramsp[:id])
+    if @user
+      render action: :show
+    else
+    render file: 'public/404', status: 404, format: [:html]
+  end
   end
 end
